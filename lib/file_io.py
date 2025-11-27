fb6bef92e3cea7021b2dc61cd7bf64b8409497cf
Kilroy-creator <royhenry877@gmail.com>
2025-11-27 14:44:08 +0300

    first

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,36 @@
 def write_file(file_name, file_content):
-    pass
+    """
+    Write content to a .txt file.
+    
+    Args:
+        file_name: The name/path of the file (without .txt extension)
+        file_content: The content to write to the file
+    """
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Append content to an existing .txt file.
+    
+    Args:
+        file_name: The name/path of the file (without .txt extension)
+        append_content: The content to append to the file
+    """
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Read and return the content of a .txt file.
+    
+    Args:
+        file_name: The name/path of the file (without .txt extension)
+    
+    Returns:
+        The content of the file as a string
+    """
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
